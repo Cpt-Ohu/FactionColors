633e0752042ba8b84377d0548b993ecb674adbb9
Cpt-Ohu <alex.ohu@gmx.at>
2017-06-23 15:54:30 +0200

    Fixing Examples

--- Source/FactionCOlors/Assembly_Races/ApparelDetailDrawer.cs
+++ Source/FactionCOlors/Assembly_Races/ApparelDetailDrawer.cs
@@ -24,7 +24,7 @@ namespace FactionColors
                     HasDetail = AppProps.DetailChance >= Rand.Range(0.1f, 0.9f);
                     //          Log.Message("CheckingDetail");
                     if (HasDetail)
-                    { 
+                    {
                         appDetailInt = AppProps.ApparelDetails.RandomElementByWeight((ApparelDetail hd) => hd.Commonality);
                         FirstSpawn = false;
                         return appDetailInt;
@@ -40,11 +40,11 @@ namespace FactionColors
         {
             get
             {
-                if(this.AppDetail!= null && this.apparel.Wearer == null)
+                if (this.AppDetail != null && this.apparel.Wearer == null)
                 {
-                    detailGraphicInt = GraphicDatabase.Get<Graphic_Multi>(AppDetail.DetailGraphicPath, ShaderDatabase.CutoutComplex, drawSize, parent.DrawColor, parent.DrawColorTwo);                    
+                    detailGraphicInt = GraphicDatabase.Get<Graphic_Multi>(AppDetail.DetailGraphicPath, ShaderDatabase.CutoutComplex, drawSize, parent.DrawColor, parent.DrawColorTwo);
                 }
-                else if(this.AppDetail != null && this.apparel.Wearer != null)
+                else if (this.AppDetail != null && this.apparel.Wearer != null)
                 {
                     string path;
                     if (this.apparel.def.apparel.LastLayer == ApparelLayer.Overhead)
@@ -75,14 +75,14 @@ namespace FactionColors
                 return (ApparelDetailProps)this.props;
             }
         }
-        
+
         private Apparel apparel
         {
             get
             {
-               return this.parent as Apparel;
+                return this.parent as Apparel;
             }
-        }        
+        }
 
         public override void PostSpawnSetup(bool respawnAfterLoad)
         {
@@ -91,91 +91,22 @@ namespace FactionColors
             if (this.DetailGraphic == null) Log.Message("NoAppGraphic");
             InitiateDetails();
         }
-       
+
 
         public void InitiateDetails()
         {
             if (FirstSpawn)
             {
                 HasDetail = AppProps.DetailChance >= Rand.Range(0.1f, 0.9f);
-      //          Log.Message("CheckingDetail");
+                //          Log.Message("CheckingDetail");
                 if (HasDetail)
                 {
                     appDetailInt = AppProps.ApparelDetails.RandomElementByWeight((ApparelDetail hd) => hd.Commonality);
-     //               this.DetailGraphic = GraphicDatabase.Get<Graphic_Multi>(AppDetail.DetailGraphicPath, ShaderDatabase.CutoutComplex, drawSize, parent.DrawColor, parent.DrawColorTwo);
-     //               Log.Message("HasDetail");
+                    //               this.DetailGraphic = GraphicDatabase.Get<Graphic_Multi>(AppDetail.DetailGraphicPath, ShaderDatabase.CutoutComplex, drawSize, parent.DrawColor, parent.DrawColorTwo);
+                    //               Log.Message("HasDetail");
                 }
             }
             FirstSpawn = false;
-        }
- 
-
-        public static bool ReturnApparelDetails(Apparel curr, out ApparelGraphicRecord result)
-        {
-            ApparelDetailDrawer drawer;
-            if((drawer = curr.TryGetComp<ApparelDetailDrawer>()) != null)
-            {
-          //      Log.Message("Checking Available Details");
-                if (drawer.HasDetail)
-                {
-        //            Log.Message("Found Detail");
-                    ApparelGraphicRecord recDetail;
-                    if (ApparelDetailDrawer.TryGetApparelDetails(curr, drawer.DetailGraphic, out recDetail))
-                    {
-          //              Log.Message("Gotten ApparelDetailRecord");
-                        result = recDetail;
-                        return true;
-                    }
-                }
-            }
-            result = new ApparelGraphicRecord();
-            return false;
-        }
-
-        public static bool TryGetApparelDetails(Apparel curr, Graphic detailgraphic, out ApparelGraphicRecord recDetail)
-        {
-  //          Log.Message("Trying to get GraphicRecord");
-            Apparel temp1 = new Apparel();
-            if (curr.def.apparel.LastLayer == ApparelLayer.Overhead)
-            {
-                temp1.def = FactionColorsDefOf.Overlay_Headgear;
-            }
-            else
-            {
-                temp1.def = FactionColorsDefOf.Overlay_Body;
-            }
- //           Log.Message("GraphicRecord of DEF: "+ temp1.def.ToString());
-            recDetail = new ApparelGraphicRecord(detailgraphic, temp1);
-            return true;
-        }
-
-
-        public static void DrawDetails(Pawn pawn, Apparel curr)
-        {
-            try
-            {
-                if (pawn.needs != null && pawn.story != null && !pawn.kindDef.factionLeader)
-                {
-                    ApparelDetailDrawer drawer;
-                    if ((drawer = curr.TryGetComp<ApparelDetailDrawer>()) != null)
-                    {
-                        drawer.PostSpawnSetup(false);
-                        if (drawer.HasDetail)
-                        {
-                            ApparelGraphicRecord recDetail;
-                            if (ApparelDetailDrawer.TryGetApparelDetails(curr, drawer.DetailGraphic, out recDetail))
-                            {
-  //                              Log.Message("Inserting Detail");
-                                pawn.Drawer.renderer.graphics.apparelGraphics.Add(recDetail);
-                            }
-                        }
-                    }
-                }
-            }
-            catch
-            {
-            }
-
         }
 
         public static bool GetDetailGraphic(Pawn pawn, Apparel curr, Rot4 bodyFacing, out Material detailGraphic)
@@ -202,9 +133,7 @@ namespace FactionColors
             {
                 return false;
             }
-
         }
-
 
         public override void PostExposeData()
         {
@@ -212,7 +141,7 @@ namespace FactionColors
             Scribe_Values.Look<bool>(ref HasDetail, "HasDetail", true, false);
             Scribe_Values.Look<bool>(ref FirstSpawn, "FirstSpawn", false, false);
             Scribe_Values.Look<string>(ref this.texPath, "texPath", null, false);
-        }     
+        }
 
     }
 }
